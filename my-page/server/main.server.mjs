a9151f4c933b8e9e8725be0c7acbb075f6142c85
cinthiabs <cinthiabarbosa8d@outlook.com>
2024-03-27 19:50:44 -0300

    Auto-generated commit

---
+++ my-page/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZB6JQAS7.mjs";import"./chunk-7DCFE5WO.mjs";import"./chunk-S543NBDL.mjs";import"./chunk-D5HSVF7Y.mjs";import"./chunk-K7YHPIF3.mjs";export{a as default};
